04e4e1fc0ab1a8340c0a58bff06d8d4507018edc
KunJon-analytics <johnadelakun1@gmail.com>
2024-06-11 10:25:31 +0100

    made stats dynamic

--- components/marketing/stats.tsx
+++ components/marketing/stats.tsx
@@ -1,21 +1,45 @@
-import { Shell } from "@/components/dashboard/shell";
-import { numberFormatter } from "@/lib/utils";
+"use client";
 
-export async function Stats() {
+import { Shell } from "@/components/dashboard/shell";
+import useStats from "@/hooks/use-stats";
+import { numberFormatter } from "@/lib/utils";
+import { Skeleton } from "../ui/skeleton";
+import { defaultStats } from "@/lib/site/stats";
+
+export function Stats() {
+  const { data, status } = useStats();
+  if (status === "pending") {
+    return (
+      <Shell>
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 sm:gap-16">
+          {new Array(3).fill(0).map((_, i) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            <Skeleton key={i} className="h-16 w-full" />
+          ))}
+        </div>
+      </Shell>
+    );
+  }
   return (
     <Shell>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 sm:gap-16">
         <div className="text-center">
-          <h3 className="font-cal text-3xl">{numberFormatter(62000000)}</h3>
+          <h3 className="font-cal text-3xl">
+            {numberFormatter(data?.predictions || defaultStats.predictions)}
+          </h3>
           <p className="font-light text-muted-foreground">Total Predictions</p>
         </div>
         <div className="text-center">
-          <h3 className="font-cal text-3xl">{numberFormatter(0)}</h3>
-          <p className="font-light text-muted-foreground">Matches Today</p>
+          <h3 className="font-cal text-3xl">
+            {numberFormatter(data?.fixtures || defaultStats.fixtures)}
+          </h3>
+          <p className="font-light text-muted-foreground">Fixtures Predicted</p>
         </div>
         <div className="text-center">
-          <h3 className="font-cal text-3xl">{numberFormatter(2400)}</h3>
-          <p className="font-light text-muted-foreground">Active users</p>
+          <h3 className="font-cal text-3xl">
+            {numberFormatter(data?.users || defaultStats.users)}
+          </h3>
+          <p className="font-light text-muted-foreground">Active Players</p>
         </div>
       </div>
     </Shell>
