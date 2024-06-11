86e7803b1d334bcf79f3d3dfb076c60d04b9ddb9
KunJon-analytics <johnadelakun1@gmail.com>
2024-06-11 15:24:13 +0100

    added metadata

--- app/shared-metadata.ts
+++ app/shared-metadata.ts
@@ -1,9 +1,8 @@
 import { siteConfig } from "@/config/site";
 import type { Metadata } from "next";
 
-export const TITLE = "FutbolPi: Predict Football Scores | Match Predictions";
-export const DESCRIPTION =
-  "Predict football scores with FutbolPi! Get match predictions, earn points, and climb the leaderboard. Join our community of passionate football fans.";
+export const TITLE = siteConfig.name;
+export const DESCRIPTION = siteConfig.description;
 
 export const defaultMetadata: Metadata = {
   title: {
@@ -18,12 +17,12 @@ export const twitterMetadata: Metadata["twitter"] = {
   title: TITLE,
   description: DESCRIPTION,
   card: "summary_large_image",
-  images: ["/api/og"],
+  images: ["/og"],
 };
 
 export const ogMetadata: Metadata["openGraph"] = {
   title: TITLE,
   description: DESCRIPTION,
   type: "website",
-  images: ["/api/og"],
+  images: ["/og"],
 };
