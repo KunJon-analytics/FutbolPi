60f318a6cad5ba32cc45393ff6f524a713be6b6e
KunJon-analytics <johnadelakun1@gmail.com>
2024-06-11 18:22:46 +0100

    added about and login page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ import { siteConfig } from "@/config/site";
 const addPathToBaseURL = (path: string) => `${siteConfig.url}${path}`;
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["/"].map((route) => ({
+  const routes = ["/", "/about", "/app/login"].map((route) => ({
     url: addPathToBaseURL(route),
     lastModified: new Date(),
   }));
