1ba67ab0c329282f839587c87b4efd605e2c9b1d
KunJon-analytics <johnadelakun1@gmail.com>
2024-06-12 05:27:11 +0100

    trying edge again

--- app/api/og/utils.ts
+++ app/api/og/utils.ts
@@ -3,22 +3,22 @@ export const SIZE = {
   height: 630,
 };
 
-export const DEFAULT_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+// export const DEFAULT_URL = process.env.VERCEL_URL
+//   ? `https://${process.env.VERCEL_URL}`
+//   : "http://localhost:3000";
 
-export const interMedium = fetch(
-  new URL("../../../public/fonts/Inter-Medium.ttf", import.meta.url),
-).then((res) => res.arrayBuffer());
+// export const interMedium = fetch(
+//   new URL("../../../public/fonts/Inter-Medium.ttf", import.meta.url),
+// ).then((res) => res.arrayBuffer());
 
-export const interRegular = fetch(
-  new URL("../../../public/fonts/Inter-Regular.ttf", import.meta.url),
-).then((res) => res.arrayBuffer());
+// export const interRegular = fetch(
+//   new URL("../../../public/fonts/Inter-Regular.ttf", import.meta.url),
+// ).then((res) => res.arrayBuffer());
 
-export const interLight = fetch(
-  new URL("../../../public/fonts/Inter-Light.ttf", import.meta.url),
-).then((res) => res.arrayBuffer());
+// export const interLight = fetch(
+//   new URL("../../../public/fonts/Inter-Light.ttf", import.meta.url),
+// ).then((res) => res.arrayBuffer());
 
 export const calSemiBold = fetch(
-  new URL("../../../public/fonts/CalSans-SemiBold.ttf", import.meta.url),
+  new URL("https://futbolpi.vercel.app/fonts/CalSans-SemiBold.ttf")
 ).then((res) => res.arrayBuffer());
