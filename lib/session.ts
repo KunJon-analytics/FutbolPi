817256ae93e869e7b6129efb1f1855058bf94f48
KunJon-analytics <johnadelakun1@gmail.com>
2024-06-17 20:25:39 +0100

    changed samestie to lax

--- lib/session.ts
+++ lib/session.ts
@@ -10,9 +10,9 @@ export const sessionOptions: SessionOptions = {
   cookieOptions: {
     // secure only works in `https` environments
     // if your localhost is not on `https`, then use: `secure: process.env.NODE_ENV === "production"`
-    secure: process.env.NODE_ENV === "production",
+    secure: env.NODE_ENV === "production",
     maxAge: undefined,
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
+    sameSite: env.NODE_ENV === "production" ? "lax" : "none",
     httpOnly: true,
   },
 };
