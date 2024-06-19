7aedfc0bfca80b675d578070589d5579080c64c1
KunJon-analytics <johnadelakun1@gmail.com>
2024-06-19 17:16:36 +0100

    fixed settings locale bug

--- app/[locale]/app/(dashboard)/settings/appearance/layout.tsx
+++ app/[locale]/app/(dashboard)/settings/appearance/layout.tsx
@@ -1,11 +1,15 @@
 import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 import { Header } from "@/components/dashboard/header";
 import { getPageBySegment } from "@/config/pages";
 
+type Props = { children: React.ReactNode; params: { locale: string } };
+
 const page = getPageBySegment(["settings", "appearance"]);
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children, params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
   const t = useTranslations("PageTitles");
 
   return (
