d3670359477ff458662954a7d1b724e18f462cfe
KunJon-analytics <johnadelakun1@gmail.com>
2024-07-31 07:47:43 +0100

    added testnet announcement

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { Hero } from "@/components/marketing/hero";
 import { BottomCTA } from "@/components/marketing/in-between-cta";
 import { Stats } from "@/components/marketing/stats";
 import { TopBallers } from "@/components/marketing/top-ballers";
+import AnnouncementBanner from "@/components/marketing/announcement-banner";
 
 type Props = {
   params: { locale: string };
@@ -15,6 +16,7 @@ export default function Home({ params: { locale } }: Props) {
   return (
     <MarketingLayout>
       <div className="grid gap-8">
+        <AnnouncementBanner />
         <Hero />
         <Stats />
         <TopBallers />
